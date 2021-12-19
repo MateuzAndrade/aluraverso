97b3dc089457c0d23476938c68d2d1ae237a926c
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2021-12-19 15:44:07 -0300

    Update conversoes.js

--- tiposVariaveisFuncoes/conversoes.js
+++ tiposVariaveisFuncoes/conversoes.js
@@ -8,7 +8,11 @@ const numeroString = "456";
 
 console.log(numero == numeroString);
 
-console.log(numero + numeroString);
 
 // conversão explícita
+//Number()
+//String()
+
+console.log(numero + Number(numeroString));
+
 
