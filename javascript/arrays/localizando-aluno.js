8723ad16d0522c9597d7efb094f93dbc35faf230
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2021-12-24 09:53:38 -0300

    Create localizando-aluno.js

---
+++ javascript/arrays/localizando-aluno.js
@@ -0,0 +1,24 @@
+const alunos = ['Paulo', 'Bia', 'Vivian', 'Isabela'];
+const mediasDoAlunos = [10, 7, 9, 6];
+
+let listaDeNotasEAlunos = [alunos, mediasDoAlunos];
+
+const exibeNomeNota = (nomeDoAluno) => {
+    if (listaDeNotasEAlunos[0].includes(nomeDoAluno)) {
+        indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno)
+        return listaDeNotasEAlunos[0][indice] + " ,sua média é " + listaDeNotasEAlunos[1][indice]+".";
+    } else {
+        return "Aluno não está cadastrado."
+    }
+}
+
+const resultado = (nota) => {
+    if (listaDeNotasEAlunos[1][indice] >= 7) {
+        return "APROVADO"
+        
+    } else {
+        return "REPROVADO"
+    }
+}
+
+console.log(exibeNomeNota("Vivian"), resultado(exibeNomeNota));
