6aa62c5a358890807cf6c583112e6ba6a18b93e4
MateusAndrade <mateus.nascimento.andrade@gmail.com>
2021-12-26 15:12:18 -0300

    ...

---
+++ javascript/objetos/objetos.js
@@ -0,0 +1,8 @@
+
+const cliente = {
+    nome:"Mateus",
+    idade: 26,
+    CPF: "123456789000",
+    email: "contato@mateusdev.com.br" 
+}
+
---
+++ javascript/objetos/print-infos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome:"Mateus",
+    idade: 26,
+    CPF: "123456789000",
+    email: "contato@mateusdev.com.br" 
+}
+
+console.log(`Informações do cliente Nome: ${cliente.nome}.`);
+console.log(cliente.CPF.substring(0,4));
