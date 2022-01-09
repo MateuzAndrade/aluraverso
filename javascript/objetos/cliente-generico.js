6f08a3175f7a7a99bf32af10e6e59c8d96629538
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-01-09 16:19:33 -0300

    Create cliente-generico.js

---
+++ javascript/objetos/cliente-generico.js
@@ -0,0 +1,10 @@
+function criarCliente (nome, idade, cpf, email, endereco) {
+
+    this.nome = nome,
+    this.idade = idade,
+    this.cpf = cpf,
+    this.email = email,
+    this.endereco = endereco;
+}
+
+criarCliente
