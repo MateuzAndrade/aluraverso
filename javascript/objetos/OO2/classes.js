cb3d8ce6e5ccb75992bc6284ea59e73c2fc4d552
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-01-17 21:02:16 -0300

    Create classes.js

---
+++ javascript/objetos/OO2/classes.js
@@ -0,0 +1,39 @@
+class ClienteCC {
+    constructor (nome, cpf, agencia, conta,_saldo, enderenco){
+        this.Nome = nome,
+        this.Cpf = cpf,
+        this.Agencia = agencia,
+        this.Conta = conta,
+        this._Saldo = _saldo,
+        this.Enderenco = enderenco
+    }
+
+    deposito (valor) {
+        if ( valor > 0) {
+            this._Saldo += valor;
+            return console.log(`Olá ${this.Nome} você realizou um deposito de R$${valor} seu saldo atual é de R$${this._Saldo}`);
+        } else {
+            console.error("ERRO NÃO SE DEPOSITA NÚMERO NEGATIVO");
+        }
+    }
+
+    saque (valor) {
+        if (valor <= this._Saldo) {
+            this._Saldo -= valor;
+            return console.log(`Olá ${this.Nome} você realizou um saque de R$${valor} seu saldo atual é de R$${this._Saldo}`);
+        } else {
+            console.error("Saldo Insuficiente");
+        }
+    }
+
+    exibirSaldo (){
+        console.log(this._Saldo);
+    }
+}
+
+const Mateus = new ClienteCC ("Mateus","00001","0000001","10101001",100,"Rua Digital");
+
+console.log(Mateus);
+
+Mateus.saque(30);
+Mateus.deposito(5689);
