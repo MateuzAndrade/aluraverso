b7c5e558529e69137569610f69c2cd8fd1b30bb6
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-02-16 21:16:16 -0300

    ...

--- nodejs/LIB-MARKDOWN/index.js
+++ nodejs/LIB-MARKDOWN/index.js
@@ -1,12 +1,23 @@
 import chalk from 'chalk';
 import * as fs from 'fs';
 
-
-function pegaArquivo (caminhoDoArquivo) {
-    const encoding = "utf8";
-    fs.readFile (caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.green(texto));
-    } )
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não existe arquivo neste caminho'));
 }
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf8';
+    fs.promises.readFile()
+}
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = "utf8";
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro)
+//         }
+//         console.log(chalk.blueBright(texto));
+//     })
+// }
 
 pegaArquivo("./arquivos/texto1.md");
