5f2cd100fdb1e842a8edc17e10a2efbab278a147
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-02-20 10:56:58 -0300

    ...

--- javascript/algoritimos1/selectionSort.js
+++ javascript/algoritimos1/selectionSort.js
@@ -1,14 +1,17 @@
-const livros = require ('./listalivros');
+const livros = require('./listalivros');
 const menorValor = require('./menorValor')
 
 for (let atual = 0; atual < livros.length; atual++) {
     let menor = menorValor(livros, atual)
 
     let livroAtual = livros[atual];
-
+    
     let livroMenorPreco = livros[menor];
 
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual
-    
+
+
 }
+
+console.log(livros);
