2aedce7879a26836abfd95770708c755d349e219
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-02-23 07:45:07 -0300

    ....

---
+++ javascript/algoritimos2/aula-1/index.js
@@ -0,0 +1,12 @@
+const { edFolha , edGalho } = require ('./arrays');
+
+function juntaListas (lista1, lista2) {
+    
+    let listaFinal = [];
+    let posicaoLista1 = 0;
+    let posicaoLista2 = 0;
+
+    while (posicaoLista1 < lista1.length && posicaoLista2 < lista2.length ){
+
+    }
+}
