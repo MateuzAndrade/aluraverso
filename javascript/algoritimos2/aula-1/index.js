c30cd464731eb4a693b891ad085dc4774c8c65e2
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-02-24 07:12:05 -0300

    .....

--- javascript/algoritimos2/aula-1/index.js
+++ javascript/algoritimos2/aula-1/index.js
@@ -6,33 +6,33 @@ const {
 function juntaListas(lista1, lista2) {
 
     let listaFinal = [];
-    let posicaoLista1 = 0;
-    let posicaoLista2 = 0;
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
     let atual = 0;
 
-    while (posicaoLista1 < lista1.length && posicaoLista2 < lista2.length) {
-        let produtoAtualLista1 = lista1[posicaoLista1];
-        let produtoAtualLista2 = lista2[posicaoLista2];
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
 
         if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
             listaFinal[atual] = produtoAtualLista1;
-            posicaoLista1++;
+            posicaoAtualLista1++;
         } else {
             listaFinal[atual] = produtoAtualLista2;
-            posicaoLista2++;
+            posicaoAtualLista2++;
         }
         atual++;
     }
-    while (posicaoLista1 < lista1.length) {
+    while (posicaoAtualLista1 < lista1.length) {
         listaFinal[atual] = lista1[posicaoAtualLista1];
         posicaoLista1++;
         atual++
     };
 
     
-    while (posicaoLista2 < lista2.length) {
+    while (posicaoAtualLista2 < lista2.length) {
         listaFinal[atual] = lista2[posicaoAtualLista2];
-        posicaoLista2++;
+        posicaoAtualLista2++;
         atual++
     };
 
---
+++ javascript/algoritimos2/aula-2/index.js
@@ -0,0 +1,37 @@
+const listaLivros = require('./array');
+
+function ordenar(parte1, parte2) {
+    let posicaoAtualParte1 = 0;
+    let posicaoAtualParte2 = 0;
+    const resultado = [];
+
+    while (posicaoAtualParte1 < parte1.length && posicaoAtualParte2 < parte2.length) {
+        let produtoAtualParte1 = parte1[posicaoAtualParte1];
+        let produtoAtualParte2 = parte2[posicaoAtualParte2];
+
+        if (produtoAtualParte1.preco < produtoAtualParte2.preco) {
+            resultado.push(produtoAtualParte1);
+            posicaoAtualParte1++;
+        } else {
+            resultado.push(produtoAtualParte2);
+            posicaoAtualParte2++;
+
+        }
+
+return resultado.concat(posicaoAtualParte1 < parte1.length ? parte1.slice(posicaoAtualParte1) : parte2.slice(posicaoAtualParte2));
+
+    }
+}
+
+function mergeSorte(array) {
+    if (array.length > 1) {
+        const metade = Math.floor(array.length / 2);
+        const parte1 = mergeSorte(array.slice(0, metade));
+        const parte2 = mergeSorte(array.slice(metade, array.length));
+
+        array = ordenar(parte1, parte2);
+    }
+    return array;
+}
+
+console.log(mergeSorte(listaLivros));
