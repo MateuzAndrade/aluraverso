bf2944d6c338fe0f4a47a921f40e4a828b73c55b
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-02-24 05:54:26 -0300

    ....

--- javascript/algoritimos2/aula-1/index.js
+++ javascript/algoritimos2/aula-1/index.js
@@ -1,12 +1,42 @@
-const { edFolha , edGalho } = require ('./arrays');
+const {
+    edFolha,
+    edGalho
+} = require('./arrays');
 
-function juntaListas (lista1, lista2) {
-    
+function juntaListas(lista1, lista2) {
+
     let listaFinal = [];
     let posicaoLista1 = 0;
     let posicaoLista2 = 0;
+    let atual = 0;
 
-    while (posicaoLista1 < lista1.length && posicaoLista2 < lista2.length ){
+    while (posicaoLista1 < lista1.length && posicaoLista2 < lista2.length) {
+        let produtoAtualLista1 = lista1[posicaoLista1];
+        let produtoAtualLista2 = lista2[posicaoLista2];
 
+        if (produtoAtualLista1.preco < produtoAtualLista2.preco) {
+            listaFinal[atual] = produtoAtualLista1;
+            posicaoLista1++;
+        } else {
+            listaFinal[atual] = produtoAtualLista2;
+            posicaoLista2++;
+        }
+        atual++;
     }
+    while (posicaoLista1 < lista1.length) {
+        listaFinal[atual] = lista1[posicaoAtualLista1];
+        posicaoLista1++;
+        atual++
+    };
+
+    
+    while (posicaoLista2 < lista2.length) {
+        listaFinal[atual] = lista2[posicaoAtualLista2];
+        posicaoLista2++;
+        atual++
+    };
+
+    return listaFinal;
 }
+
+console.log(juntaListas(edGalho, edFolha));
