4fa65840134c5a689544d092daee7cb8ef9ddee3
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-05 19:47:36 -0300

    ....

--- javascript/heranca/SistemaAutenticacao.js
+++ javascript/heranca/SistemaAutenticacao.js
@@ -1,5 +1,5 @@
 export class SistemaAutenticacao {
     static login(funcionario, senha) {
-        return funcionario.senha == senha;
+        return funcionario._senha == senha;
     }
 }
--- javascript/heranca/index.js
+++ javascript/heranca/index.js
@@ -1,6 +1,7 @@
 import { Gerente } from "./funcionarios/Gerente.js";
 import { Diretor } from "./funcionarios/diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+import { Cliente } from "./Cliente.js";
 
 
 const diretor = new Diretor("Rodrigo", 10000, 1234564789);
@@ -8,5 +9,10 @@ diretor.cadastrarSenha("123456789");
 const gerente = new Gerente("Ricardao", 5000, 1346479846);
 
 const estalogado = SistemaAutenticacao.login(diretor, "123456789");
+const cliente = new Cliente ("Lais", 78945613, "456");
 
-console.log( estalogado );
+
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "123456789");
+
+
+console.log( clienteEstaLogado );
