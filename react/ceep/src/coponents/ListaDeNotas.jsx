f12fff001b6eee4891be25c27593e201192415fc
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-07 06:44:25 -0300

    ...

---
+++ react/ceep/src/coponents/ListaDeNotas.jsx
@@ -0,0 +1,13 @@
+
+export class ListaDeNotas extends component {
+    render(){
+    <ul>
+    <li>
+        <section>
+            <h3>Título</h3>
+            <p>Escreva sua nota</p>
+        </section>
+    </li>
+</ul>
+}
+}
