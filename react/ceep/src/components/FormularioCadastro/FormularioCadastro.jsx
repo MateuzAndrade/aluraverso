ce18994cf8c442b16d6405f0c54ec77add47208c
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-09 22:42:37 -0300

    .

--- react/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ react/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
-import './style.css'
+import "./style.css";
 
 class FormularioCadastro extends Component {
+  constructor(props) {
+    super(props);
+    this.titulo = "";
+    this.texto = "";
+  }
 
-  handleMudancaTitulo(evento){
-    console.log(evento.target.value);
+  _handleMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+  }
+
+  _handleMudancaTexto(evento) {
+    this.texto = evento.target.value;
+  }
+
+  _criarNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation()
+    this.props.criarNota(this.titulo, this.texto)
   }
 
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
-          onChange={this.handleMudancaTitulo}
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
@@ -29,5 +45,4 @@ class FormularioCadastro extends Component {
   }
 }
 
-
-export default  FormularioCadastro
+export default FormularioCadastro;
