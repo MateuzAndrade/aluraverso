1979bdc90e8fcb14421c3dfc0d49150ba9fd6254
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-12 14:45:26 -0300

    ...

---
+++ csharp/AprendendoCSharp/7-Condicionais/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Executando projeto 6");
+
+int notaDoAluno = 5;
+
+if (notaDoAluno >= 7)
+{
+    Console.WriteLine("Aluno Aprovado");
+}
+else 
+{
+    Console.WriteLine("Aluno Reprovado");
+}
+
+
+Console.WriteLine("A execuçaõ terminou");
+Console.ReadLine();
