f12c0a20fa05ca71858742bfcf3e054097eb0857
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-11 22:05:48 -0300

    ...

---
+++ csharp/AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Executando projeto 5");
+
+
+
+string titulo = "Alura cursos de Tecnologia" + 2020;
+Console.WriteLine(titulo);
+
+string cursosProgramacao = @" - .Net
+  - Java";
+
+
+
+Console.WriteLine(cursosProgramacao);
+
+Console.WriteLine("A execuçaõ terminou");
+Console.ReadLine();
---
+++ csharp/AprendendoCSharp/6 -AtribuiçõesDeVariaveis/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Executando projeto 6");
+
+int idade = 20;
+int idadeGustavo = idade;
+
+idade = 33;
+
+Console.WriteLine(idade);   
+Console.WriteLine(idadeGustavo);
+
+
+Console.WriteLine("A execuçaõ terminou");
+Console.ReadLine();
