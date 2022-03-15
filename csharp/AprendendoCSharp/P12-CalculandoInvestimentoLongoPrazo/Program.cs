1b0a214471d21b0ba06fb6ffa5766a87fcaff7e4
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-14 21:12:25 -0300

    ifbreak

---
+++ csharp/AprendendoCSharp/P12-CalculandoInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Executando o Projeto 12 - Calculo de Rendimento Poupança com For em Anos");
+
+double valorInvestido;
+valorInvestido = 1000;
+
+double fatorDeRendimento = 1.0036;
+
+for (int contadorAno = 1;contadorAno  <= 5; contadorAno ++)
+{
+    for (int contadorDeMes = 1; contadorDeMes <= 12; contadorDeMes++)
+    {
+        valorInvestido *=  fatorDeRendimento;
+    }
+    fatorDeRendimento += 0.0010;
+}
+
+Console.WriteLine(valorInvestido); 
+
+Console.WriteLine("A Execução Terminou!");
+Console.ReadLine();
---
+++ csharp/AprendendoCSharp/P13-ForEncadeado/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Executando o Projeto 13 - Laços Encadeados");
+
+for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+{
+    for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+    {
+        Console.Write("*");
+        if (contadorColuna >= contadorLinha)
+        {
+            break;
+        }
+    }
+    Console.WriteLine();
+       
+}
+
+
+Console.WriteLine("A Execução Terminou!");
+Console.ReadLine();
