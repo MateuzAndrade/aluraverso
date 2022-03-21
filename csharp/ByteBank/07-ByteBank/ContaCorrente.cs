fe0bd2291f8f0012017d8904f01e4cfc78b5b815
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-21 07:06:25 -0300

    construtores

---
+++ csharp/ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,86 @@
+﻿namespace _07_ByteBank 
+{
+    public class ContaCorrente
+    {
+        public Cliente? Titular { get; set; }
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }
+
+        private double _saldo = 100;
+
+        public double Saldo
+        {
+            get 
+            { 
+                return _saldo; 
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    _saldo = value;
+                }
+
+            }
+        }
+
+        public static int TotalDeContasCriadas 
+
+        { 
+            get;private set;
+        }
+
+        public ContaCorrente(int Agencia, int NumeroConta)
+        {
+            this.Agencia = Agencia;
+            this.NumeroConta = NumeroConta;
+
+            TotalDeContasCriadas++;
+        }
+
+        public bool Sacar(Double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                _saldo += valor;
+
+            }
+            else
+            {
+                Console.WriteLine("Deposito de numero negativo não é valido");
+            }
+        }
+
+        public bool Transfererir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo >= valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+
+}
---
+++ csharp/ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(3658,5269);
+            Console.WriteLine(conta.Agencia);
+            Console.WriteLine(conta.NumeroConta);
+
+            Console.WriteLine(ContaCorrente.TotalDeContasCriadas);
+        }
+
+       
+    }
+}
