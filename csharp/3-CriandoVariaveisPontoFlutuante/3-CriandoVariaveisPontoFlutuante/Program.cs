180c64463da08dff285fe2c2e4a93c0a21566f60
MateusAndrade <86625622+MateuzAndrade@users.noreply.github.com>
2022-03-22 07:13:07 -0300

    poo2

--- csharp/3-CriandoVariaveisPontoFlutuante/3-CriandoVariaveisPontoFlutuante/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("Executando o Projeto 3 Criando Variaveis Ponto Flutuante!");
-
-double salario;
-salario = 1200.70;
-
-Console.WriteLine(salario);
-
-Console.WriteLine("Aexecução terminou");
-Console.ReadLine();
---
+++ csharp/ByteBankPOO2/ByteBankPOO2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using ByteBankPOO2.Funcionarios;
+
+namespace ByteBankPOO2
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "123.456.789-00";
+            carlos.Salario = 3000;
+
+            
+
+        }
+    }
+}
